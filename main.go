f54a3d5fcc5935e4a2f09f6c601ac0610d87c9b3
Agustin <esparta.01.af@gmail.com>
2025-03-04 12:43:04 -0300

    example of added environment variables

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"log"
+	"os"
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/joho/godotenv"
@@ -11,46 +12,54 @@ import (
 )
 
 type Repository struct {
-	DB *gorm.DB
+    DB *gorm.DB
 }
 
 // Rutas
 func (r *Repository) SetupRoutes(app *fiber.App) {
-	api := app.Group("/api")
-	_ = api // Solo para evitar que el compilador marque esto como no usado
+    api := app.Group("/api")
+    _ = api // Solo para evitar que el compilador marque esto como no usado
 }
 
 func main() {
-	// Cargar variables de entorno
-	err := godotenv.Load(".env")
-	if err != nil {
-		log.Fatal("Error al cargar el archivo .env:", err)
-	}
+    // Cargar variables de entorno
+    err := godotenv.Load(".env")
+    if err != nil {
+        log.Fatal("Error al cargar el archivo .env:", err)
+    }
 
-	// Configuración de la base de datos
-	dsn := "host=localhost user=postgres password=111201 dbname=stock-management port=5432 sslmode=disable"
-	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
-	if err != nil {
-		log.Fatal("No se pudo conectar a la base de datos:", err)
-	}
+    // Obtener las variables de entorno
+    dbHost := os.Getenv("DB_HOST")
+    dbUser := os.Getenv("DB_USER")
+    dbPassword := os.Getenv("DB_PASSWORD")
+    dbName := os.Getenv("DB_NAME")
+    dbPort := os.Getenv("DB_PORT")
 
-	// Obtener la conexión *sql.DB de GORM para hacer ping
-	sqlDB, err := db.DB()
-	if err != nil {
-		log.Fatal("Error al obtener la conexión de base de datos:", err)
-	}
+    // Configuración de la base de datos con las variables de entorno
+    dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable", dbHost, dbUser, dbPassword, dbName, dbPort)
 
-	// Verificar la conexión con Ping
-	err = sqlDB.Ping()
-	if err != nil {
-		log.Fatal("No se pudo conectar a la base de datos:", err)
-	} else {
-		fmt.Println("¡Conexión exitosa a PostgreSQL!")
-	}
+    db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+    if err != nil {
+        log.Fatal("No se pudo conectar a la base de datos:", err)
+    }
 
-	// Configuración de Fiber
-	r := Repository{DB: db}
-	app := fiber.New()
-	r.SetupRoutes(app)
-	log.Fatal(app.Listen(":3000"))
+    // Obtener la conexión *sql.DB de GORM para hacer ping
+    sqlDB, err := db.DB()
+    if err != nil {
+        log.Fatal("Error al obtener la conexión de base de datos:", err)
+    }
+
+    // Verificar la conexión con Ping
+    err = sqlDB.Ping()
+    if err != nil {
+        log.Fatal("No se pudo conectar a la base de datos:", err)
+    } else {
+        fmt.Println("¡Conexión exitosa a PostgreSQL!")
+    }
+
+    // Configuración de Fiber
+    r := Repository{DB: db}
+    app := fiber.New()
+    r.SetupRoutes(app)
+    log.Fatal(app.Listen(":3000"))
 }
